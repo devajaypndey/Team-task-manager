98208f023c8097c1ff0dd12d0fc3c15f1438d90b
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-05-01 00:22:37 +0530

    update: Environment variable client side

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.BACKEND_API_URL || "/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
